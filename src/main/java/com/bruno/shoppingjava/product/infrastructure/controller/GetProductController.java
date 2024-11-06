d51c21e8bfb11a63875be80b419655592ca02e36
Bruno Diaz <brunjo12@hotmail.com>
2024-11-05 23:33:21 -0500

    feat: add product abstract controller

--- src/main/java/com/bruno/shoppingjava/product/infrastructure/controller/GetProductController.java
+++ src/main/java/com/bruno/shoppingjava/product/infrastructure/controller/GetProductController.java
@@ -7,15 +7,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
 @RequiredArgsConstructor
-public class GetProductController {
+public class GetProductController extends ProductAbstractController {
 
   private final GetAllProductUseCase getAllProductUseCase;
   private final GetProductUseCase getProductUseCase;
---
+++ src/main/java/com/bruno/shoppingjava/product/infrastructure/controller/ProductAbstractController.java
@@ -0,0 +1,7 @@
+package com.bruno.shoppingjava.product.infrastructure.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/products")
+public class ProductAbstractController {
+}
