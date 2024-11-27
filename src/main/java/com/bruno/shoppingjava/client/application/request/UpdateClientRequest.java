27026b0aec0de97531ba97f29c52344ef245d895
Bruno Josue Diaz Floreano <brunjo12@hotmail.com>
2024-11-27 00:07:55 -0500

    feat: remove library orika mapper

--- src/main/java/com/bruno/shoppingjava/client/application/request/UpdateClientRequest.java
+++ src/main/java/com/bruno/shoppingjava/client/application/request/UpdateClientRequest.java
@@ -1,12 +1,8 @@
 package com.bruno.shoppingjava.client.application.request;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 
 @SuperBuilder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UpdateClientRequest extends CreateClientRequest {
 }
--- src/main/java/com/bruno/shoppingjava/shared/application/mapper/MapperContext.java
+++
@@ -1,5 +0,0 @@
-package com.bruno.shoppingjava.shared.application.mapper;
-
-public interface MapperContext {
-  <T> T map(Object source, Class<T> target);
-}
