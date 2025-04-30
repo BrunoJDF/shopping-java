5cc2af5d447570a8d92c98d993b06b4e47e6cdf3
Bruno Diaz Floreano <brunjo12@hotmail.com>
2025-04-30 00:35:38 -0500

    feat: replace db_host and db_port by db_uri

--- src/main/java/com/bruno/shoppingjava/shared/infrastructure/persistence/SQLConstants.java
+++ src/main/java/com/bruno/shoppingjava/shared/infrastructure/persistence/SQLConstants.java
@@ -6,5 +6,5 @@ import org.springframework.beans.factory.annotation.Value;
 @UtilityClass
 public class SQLConstants {
   @Value("${shopping-cart-properties.sql.schema-context}")
-  public static final String SCHEMA = "shopping_cart";
+  public final String SCHEMA = "shopping_cart";
 }
